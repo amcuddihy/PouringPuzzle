a81629cb51be12611bf181931adbfeb547a15886
amcuddihy <alexcuddihy@gmail.com>
2026-05-04 11:06:48 -0400

    Fixed the setup vessel allowing a start value greater than the max and less than zero.
    Also set a limit of 999 for vessel size.

--- PouringPuzzle/Models/SetupVessel.cs
+++ PouringPuzzle/Models/SetupVessel.cs
@@ -4,6 +4,11 @@ public class SetupVessel
 {
     public event EventHandler? ValueChanged;
 
+    private const int MAX_SIZE = 999; // Arbitrary limit to prevent UI issues and performance problems. Even at this limit, the vessel
+                                      // renders impractically large in the UI. Int.MaxValue would be completely unmanageable. I considered
+                                      // setting it even lower, such as 99, but I don't want to prevent users from creating puzzles with
+                                      // large vessels if they really want to. The UI won't render them well, but it also won't break. 
+
     public int MaxValue {
         get {
             return _maxValue;
@@ -12,7 +17,7 @@ public class SetupVessel
             if (value == _maxValue) {
                 return;
             }
-            _maxValue = value;
+            _maxValue = Math.Clamp(value, 0, MAX_SIZE);
             ValueChanged?.Invoke(this, EventArgs.Empty);
         }
     }
@@ -25,7 +30,7 @@ public class SetupVessel
             if (value == _startingValue) {
                 return;
             }
-            _startingValue = value;
+            _startingValue = Math.Clamp(value, 0, _maxValue);
             ValueChanged?.Invoke(this, EventArgs.Empty);
         }
     }
